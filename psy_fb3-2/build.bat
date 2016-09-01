89a5e6ae7f6e5b53cf577763477e95a746bc5b63
ParkSeongYeolAAA <parkmar5@naver.com>
2016-09-01 17:27:05 +0900

    psy_fb3-2 first commit

---
+++ psy_fb3-2/build.bat
@@ -0,0 +1,6 @@
+rm a.exe lex.yy.c lex.yy.o psy_fb3-2.tab.c psy_fb3-2.tab.h psy_fb3-2.tab.o
+ 
+bison -d psy_fb3-2.y
+flex psy_fb3-2.l
+gcc -c psy_fb3-2.tab.c lex.yy.c psy_funcs.c
+gcc psy_fb3-2.tab.o lex.yy.o psy_funcs.o -o a.exe
